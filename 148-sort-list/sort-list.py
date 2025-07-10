d80f25c91404c32270d395a277cdeaa74cd2a52e
Sannitha Telliboina <sannithatelliboina@gmail.com>
2025-07-10 10:43:34 +0530

    Time: 191 ms (36.95%) | Memory: 32.9 MB (40.80%) - LeetSync

--- 148-sort-list/sort-list.py
+++ 148-sort-list/sort-list.py
@@ -3,16 +3,39 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+def splitList(head: ListNode) -> ListNode:
+    slow = head
+    fast = head
+    prev = None
+    while fast and fast.next:
+        prev = slow
+        slow = slow.next
+        fast = fast.next.next
+    if prev:
+        prev.next = None
+    return slow
+def mergeTwoLists(l1: ListNode, l2: ListNode) -> ListNode:
+    dummy = ListNode()
+    tail = dummy
+    while l1 and l2:
+        if l1.val < l2.val:
+            tail.next = l1
+            l1 = l1.next
+        else:
+            tail.next = l2
+            l2 = l2.next
+        tail = tail.next
+    tail.next = l1 if l1 else l2
+    return dummy.next
 class Solution:
-    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        temp = head
-        l = []
-        while temp:
-            l.append(temp.val)
-            temp = temp.next
-        l.sort()
-        temp = head
-        for val in l:
-            temp.val = val
-            temp = temp.next
-        return head
+    def sortList(self, head: ListNode) -> ListNode:
+        if not head or not head.next:
+            return head
+        mid = splitList(head)           
+        left = self.sortList(head)      
+        right = self.sortList(mid)       
+        return mergeTwoLists(left, right)
